aaee12f8353e1cb5295e649c511b543b9f0a3d3a
Istak Muhammad <44334566+imistakj@users.noreply.github.com>
2021-05-29 15:20:11 +0530

    added bootstrap data closes #13.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -3,11 +3,7 @@ package guru.springframework.sfgpetclinic.bootstrap;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
-import guru.springframework.sfgpetclinic.services.PetService;
 import guru.springframework.sfgpetclinic.services.VetService;
-import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
-import guru.springframework.sfgpetclinic.services.map.PetServiceMap;
-import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,12 +11,11 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
-
     private final VetService vetService;
 
-    public DataLoader(){
-          ownerService = new OwnerServiceMap();
-          vetService= new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
@@ -42,14 +37,14 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Owner Loaded ----------");
 
-        Vet vet1= new Vet();
+        Vet vet1 = new Vet();
         vet1.setId(1L);
         vet1.setFirstName("sam");
         vet1.setLastName("axe");
 
         vetService.save(vet1);
 
-        Vet vet2= new Vet();
+        Vet vet2 = new Vet();
         vet2.setId(2L);
         vet2.setFirstName("Jessica");
         vet2.setLastName("Porter");
@@ -57,7 +52,6 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
         System.out.println("Vets are loaded --------");
-
 
 
     }
